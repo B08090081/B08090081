c23c4c9a6e3cf1ef9d9047c689108c25b40a9100
B08090081 <74149511+B08090081@users.noreply.github.com>
2020-11-09 01:37:37 +0800

    Update 4.1mydb.php

--- 4.1mydb.php
+++ 4.1mydb.php
@@ -1,9 +1,10 @@
 <?php
     $conn = mysqli_connect("localhost", "root", "", "mydb");
-    
+     //連接mysql數據庫中的mydb資料庫
     $result=mysqli_query($conn, "select * from user");
-
+//引用上一個$conn資料庫中的連結打開from user
     $row=mysqli_fetch_array($result);
 
     echo $row[id] + " " + $row[pwd]; 
+//引用$result資料庫內中的from user資料 再使用echo $row顯示單筆資料
 ?>
